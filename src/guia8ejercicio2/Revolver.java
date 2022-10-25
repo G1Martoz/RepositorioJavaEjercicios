55c71083305b02f04ac4c0a89dc32aee297cce8e
Martin Ozuna <g1martinozuna@gmail.com>
2022-10-25 19:55:18 -0300

    ESTE ES EL EJERCICIO 2 DE LA GUIA 8 COMENTADO

--- src/guia8ejercicio2/Revolver.java
+++ src/guia8ejercicio2/Revolver.java
@@ -40,6 +40,7 @@ public class Revolver {
         this.setPosicionAgua(aux.intValue());
     }
 
+    
     public boolean mojar() {
         //si posicion actual coincide con posicion de agua es true
         if (this.posicionAgua == this.posicionActual) {
